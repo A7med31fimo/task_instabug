62c10a5942624b4a0a6afdd6c508bdddc9127130
A7med31fimo <ahmedfahim5435644@gmail.com>
2022-05-25 17:02:51 +0200

    clean my code to make it readable

--- App.js
+++ App.js
@@ -1,40 +1,35 @@
 
-import { StyleSheet, Text, View, ScrollView, FlatList, Image,ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList, Image, ActivityIndicator } from 'react-native';
 import axios from 'axios';
-import React,{ useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 export default function App() {
 
-  const fetchData = async(pNUMBER) => {
+  const fetchData = async (pNUMBER) => {
     setLoading(true);
     const baseURL = "http://api.themoviedb.org/3/discover/movie?api_key=acea91d2bff1c53e6604e4985b6989e2&page="
- await axios.get(baseURL + `${pNUMBER}`).then((response) => {
+    await axios.get(baseURL + `${pNUMBER}`).then((response) => {
       setLoading(false)
-      setData(data.concat(response.data.results))    
+      setData(data.concat(response.data.results))
     }
     )
-     
-  } 
+  }
   React.useEffect(() => {
     LoadData()
-  
-    return () => {
-      
-    }
   }, [])
 
-  const LoadData =  () => {
-      fetchData(number)
-      setNumber(number+1)
+  const LoadData = () => {
+    fetchData(number)
+    setNumber(number + 1)
   }
   const [data, setData] = useState([])
   const [number, setNumber] = useState(1)
   const [isLoading, setLoading] = useState(false);
 
-  const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+  const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
     const paddingToBottom = 20;
-    console.log(layoutMeasurement.height)
-    console.log(contentOffset.y)
-    console.log(contentSize.height)
+    // console.log(layoutMeasurement.height)
+    // console.log(contentOffset.y)
+    // console.log(contentSize.height)
     return layoutMeasurement.height + contentOffset.y >=
       contentSize.height - paddingToBottom;
   };
@@ -45,41 +40,30 @@ export default function App() {
         <Text>List of Movies</Text>
       </View>
       <View style={styles.body}>
-        <ScrollView onScroll={({nativeEvent}) => {
-      if (isCloseToBottom(nativeEvent)) {
-        LoadData()
-      }
-    }}
-    scrollEventThrottle={400}>
-        {data.map((e, i) => (
-            
-            <View key={i} style={{
-              borderRadius: 8, borderColor: "#574B44", borderWidth: 2
-            }}>
-           
+        <ScrollView onScroll={({ nativeEvent }) => {
+          if (isCloseToBottom(nativeEvent)) {
+            LoadData()
+          }
+        }}
+          scrollEventThrottle={400}>
+          {data.map((e, i) => (
+            <View key={i} style={styles.item}>
+
               <Text style={{ fontSize: 18, color: "#570C08" }}>Title :{e.title}</Text>
               <Text>OverView :{e.overview}</Text>
-              <Image source={{
-                uri: "https://image.tmdb.org/t/p/original" + e.poster_path
-              }} style={{
-                width: 200,
-                height: 200, borderRadius: 10
+              <Image source={{ uri: "https://image.tmdb.org/t/p/original" + e.poster_path }} style={styles.img} />
+              <Text style={{ fontSize: 16, color: "#FFFFFF", fontStyle: 'italic' }}>Release_date :{e.release_date}</Text>
+              {isLoading ?
+                <View style={styles.indicator }>
+                  <ActivityIndicator size="large" color={"#ff0000"} />
+                </View> : null
 
-              }}
-              ></Image>
-              <Text style={{ fontSize: 16, color: "#FFFFFF", fontStyle: 'italic' }}>Release_date :{e.release_date}
-              </Text>
-              { isLoading?
-   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator size="large" color={"#ff0000"}/>
-  </View>:null
-  
-  }
+              }
             </View>))
-            
+
           }
         </ScrollView>
-        
+
       </View>
       <View style={styles.footer}></View>
     </View>
@@ -101,5 +85,16 @@ const styles = StyleSheet.create({
   }, body: {
     flex: 6,
     backgroundColor: "#425654",
-  },
-});
+  }, img: {
+    width: 200,
+    height: 200, borderRadius: 10
+  }
+  , indicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },item:{
+    borderRadius: 8, borderColor: "#574B44", borderWidth: 2
+  }
+}
+);
